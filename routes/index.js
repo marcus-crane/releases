e3b23ed1529c3962bf9322c658659642aa427b5a
Marcus <marcus@thingsima.de>
2017-01-09 15:51:12 +1300

    Dont need env either. This is just because my server isn\'t liking it.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const path = require('path')
-const dotenv = require('dotenv').config()
 const moment = require('moment')
 
 const Knex = require('knex');
