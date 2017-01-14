54fc5a7bea878901a9b8dadf3d3a5924f2a9b3c3
Marcus <marcus@thingsima.de>
2017-01-15 01:45:58 +1300

    Added a custom user agent

--- utils/gb.js
+++ utils/gb.js
@@ -2,15 +2,27 @@ const axios = require('axios')
 const env = require('dotenv').config({ path: '../.env' })
 
 const queryByName = (title) => {
-    return axios.get(`http://www.giantbomb.com/api/search?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&query=${title}&resources=game`)
+    return axios.get(`http://www.giantbomb.com/api/search?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&query=${title}&resources=game`, {
+        headers: {
+            'User-Agent': 'Releases/0.2 (+http://releases.thingsima.de)'
+        }
+    })
 }
 
 const queryByID = (game_id) => {
-    return axios.get(`http://www.giantbomb.com/api/game/3030-${game_id}/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=date_last_updated,deck,developers,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year,id,images,name,platforms,publishers,releases`)
+    return axios.get(`http://www.giantbomb.com/api/game/3030-${game_id}/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=date_last_updated,deck,developers,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year,id,images,name,platforms,publishers,releases`, {
+        headers: {
+            'User-Agent': 'Releases/0.2 (+http://releases.thingsima.de)'
+        }
+    })
 }
 
 const queryRelease = (game_id) => {
-    return axios.get(`http://www.giantbomb.com/api/release/3050-${game_id}/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=date_last_updated,name,platform,region,developers,publishers,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year`)
+    return axios.get(`http://www.giantbomb.com/api/release/3050-${game_id}/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=date_last_updated,name,platform,region,developers,publishers,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year`, {
+        headers: {
+            'User-Agent': 'Releases/0.2 (+http://releases.thingsima.de)'
+        }
+    })
 }
 
 module.exports = {
