de8c5252b50828ec09a83b8d6a49b1973ccc2c36
Marcus Crane <marcus@thingsima.de>
2017-02-25 21:20:33 +1300

    Updated deps and fixed importing new titles

--- routes/backend.js
+++ routes/backend.js
@@ -65,15 +65,19 @@ router.get('/add/:id', (req, res, next) => {
       entry.gb_id = game.data.results.id
       entry.developer = game.data.results.developers[0].name
       entry.publisher = game.data.results.publishers[0].name
-      entry.date = moment(`${game.data.results.original_release_date} GMT`)
+      entry.date = moment(`
+          ${game.data.results.expected_release_year}-
+          ${game.data.results.expected_release_month}-
+          ${game.data.results.expected_release_day}
+          `)
       entry.bgcover = game.data.results.image.medium_url
       entry.description = game.data.results.deck
 
       res.render('confirm', { 'game': entry, 'header': 'Confirm details', 'tagline': `Here's what we could find about ${entry.name}` })
     })
     .catch((err) => {
-      console.log(`Failed to fetch ${req.params.title}`, err)
-      res.send(`Oops, couldn't fetch ${req.params.title}! Check the terminal.`)
+      console.log(`Failed to fetch ${req.params.id}`, err)
+      res.send(`Oops, couldn't fetch ${req.params.id}! Check the terminal.`)
     })
 })
 
